6109d90727ee4f573ea763f0a279a92848358015
Zaver85 <zaver85.com@maol.ru>
2024-03-14 11:33:53 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int m = 3;
+        int n = 2;
+
+        int result = AckermannFunction(m, n);
+
+        Console.WriteLine($"A({m}, {n}) = {result}");
+    }
+
+    static int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+            return n + 1;
+        else if (m > 0 && n == 0)
+            return AckermannFunction(m - 1, 1);
+        else
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    }
+}
