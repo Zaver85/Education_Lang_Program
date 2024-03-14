a18e856a6b5b869fbdc3081cc1dce2ea1e489987
Zaver85 <zaver85.com@maol.ru>
2024-03-14 11:35:32 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void ReversePrint(int[] arr, int idx)
+    {
+        if (idx < 0)
+            return;
+
+        Console.Write(arr[idx] + " ");
+        ReversePrint(arr, idx - 1);
+    }
+
+    static void Main(string[] args)
+    {
+        int[] myArray = { 1, 2, 5, 0, 10, 34 };
+
+        ReversePrint(myArray, myArray.Length - 1);
+    }
+}
